e56ed8cdde87375a0c036dfd81910ef31364bf7e
Jakob Elias Wagner <jakob.e.wagner@tum.de>
2023-06-27 17:05:16 +0200

    add: boundary tests

--- test/layer_tests.py
+++ test/layer_tests.py
@@ -28,3 +28,39 @@ class ScaleLayerTestCase(unittest.TestCase):
             right_cond = torch.all(torch.less_equal(out, min(after_bv)))
 
             self.assertTrue(left_cond and right_cond)
+
+    def test_left_boundary(self):
+        n = 100
+        for _ in range(n):
+            # setup layer
+            before_bv = np.random.random((2,))
+            after_bv = np.random.random((2,))
+            layer = ScaleLayer(
+                min(before_bv), max(before_bv), min(after_bv), max(after_bv)
+            )
+
+            # pass tensor through layer
+            x = torch.Tensor([min(before_bv)], dtype=torch.float32)
+            out = layer(x)
+
+            cond = torch.equal(out, min(after_bv))
+
+            self.assertTrue(cond)
+
+    def test_right_boundary(self):
+        n = 100
+        for _ in range(n):
+            # setup layer
+            before_bv = np.random.random((2,))
+            after_bv = np.random.random((2,))
+            layer = ScaleLayer(
+                min(before_bv), max(before_bv), min(after_bv), max(after_bv)
+            )
+
+            # pass tensor through layer
+            x = torch.Tensor([max(before_bv)], dtype=torch.float32)
+            out = layer(x)
+
+            cond = torch.equal(out, max(after_bv))
+
+            self.assertTrue(cond)
